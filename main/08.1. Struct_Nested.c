d29e8ad7737f2a45f51b44279d8d4e463bb683e9
thanhtranndang <thanhtranndang@gmail.com>
2023-10-05 23:37:50 +0700

    first commit

---
+++ main/08.1. Struct_Nested.c
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+// Cấu trúc lồng nhau
+
+struct Date
+{
+	int day;
+	int month;
+	int year;
+
+};
+
+struct Student
+{
+	struct Date Birthday;
+	char name[100];
+	int mssv;
+
+};
+
+
+void Input(struct Student* student) {
+	printf("Nhap ten sinh vien: ");
+	fgets(student->name,20, stdin);
+	student->name[strlen(student->name) - 1] = '\0';
+
+	printf("Nhap ngay sinh: ");
+	scanf("%d %d %d ", &student->Birthday.day, &student->Birthday.month, &student->Birthday.year);
+	
+	printf("Nhap ma so sinh vien: ");
+	scanf("%d", &student->mssv);
+}
+
+void disPlay(struct Student student) {
+	printf("Ten: %s\n", student.name);
+	printf("Ngay sinh: %02d/%02d/%04d\n", student.Birthday.day, student.Birthday.month, student.Birthday.year);
+	printf("Ma so sinh vien: %d\n", student.mssv);
+}
+
+
+int main() {
+
+	struct Student sv1;
+	Input(&sv1);
+	disPlay(sv1);
+	return 0;
+}
